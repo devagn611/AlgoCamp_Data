e34957dfcd1fe490ad8da29a077e379db6b415d4
Devagn Maniya <99108074+devagn611@users.noreply.github.com>
2024-09-14 13:41:41 +0530

    multiplayer_upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Button() {
+  return (
+        <button className="btn btn-primary rounded-sm text-white bg-blue-700">Click Me</button>
+    );
+}
+
+export default Button;
